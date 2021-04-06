ac963afa6d74c1b85810552f96521514c970a85f
Ken Youens-Clark <kyclark@gmail.com>
2021-04-06 13:06:19 -0700

    done with first draft

--- copy_db.sh
+++ copy_db.sh
@@ -15,7 +15,8 @@ dropdb --if-exists $DEST_DB && createdb $DEST_DB
 pg_dump --schema-only $SRC_DB | psql $DEST_DB
 
 # Tables safe to copy as is
-COPY="batch drug_class drug dose_unit ethnicity apoe race country time_unit arm"
+COPY="batch drug_class drug dose_unit ethnicity apoe race country time_unit \
+      lab_unit lab_test arm"
 for TABLE in $COPY; do
     echo "Copying \"$TABLE\""
     pg_dump --data-only $SRC_DB -t $TABLE | psql $DEST_DB
--- export.py
+++ export.py
@@ -96,6 +96,38 @@ def main() -> None:
                         src_med, dst_ssa, dst_subject_id)
                     print(f'\t\t\tMED {j}: {dst_med.drug.name}')
 
+                visits = src.SubjectVisit.select().where(
+                    src.SubjectVisit.subject_study_arm_id ==
+                    src_ssa.subject_study_arm_id)
+
+                for src_visit in visits:
+                    dst_visit = create_subject_visit(src_visit, dst_ssa)
+                    print(f'\t\t\tVISIT {j}: {dst_visit.code}')
+
+                    vitals = src.VisitVitals.select().where(
+                        src.VisitVitals.subject_visit_id ==
+                        src_visit.subject_visit_id)
+
+                    for src_vital in vitals:
+                        dst_vital = create_visit_vital(src_vital, dst_visit)
+                        print(f'\t\t\t\tVITALS {j}: '
+                              f'{dst_vital.visit_vitals_id}')
+
+                    mmse = src.Mmse.select().where(src.Mmse.subject_visit_id ==
+                                                   src_visit.subject_visit_id)
+
+                    for src_mmse in mmse:
+                        dst_mmse = create_mmse(src_mmse, dst_visit)
+                        print(f'\t\t\t\tMMSE {j}: ' f'{dst_mmse.mmse_id}')
+
+                    lab_results = src.LabResult.select().where(
+                        src.LabResult.subject_visit_id ==
+                        src_visit.subject_visit_id)
+
+                    for src_lab_result in lab_results:
+                        dst_lab = create_lab_result(src_lab_result, dst_visit)
+                        print(f'\t\t\t\tLAB {j}: ' f'{dst_lab.result}')
+
     print('Done.')
 
 
@@ -170,6 +202,110 @@ def create_subject_medication(src_med, dst_ssa, dst_subject_id):
         record_num=src_med.record_num)
 
     return dst_med
+
+
+# --------------------------------------------------
+def create_subject_visit(src_visit, dst_ssa):
+    """ Create a subject visit """
+
+    dst_med, _ = dst.SubjectVisit.get_or_create(
+        subject_visit_id=src_visit.subject_visit_id,
+        subject_study_arm_id=dst_ssa.subject_study_arm_id,
+        ad_stage_id=src_visit.ad_stage_id,
+        visit_offset=src_visit.visit_offset,
+        code=src_visit.code,
+        code2=src_visit.code2,
+        description=src_visit.description,
+        duration=src_visit.duration,  # noise?
+        time_unit_id=src_visit.time_unit_id,
+        visit_type=src_visit.visit_type,
+        visit_sequence=src_visit.visit_sequence,
+        tier_level=src_visit.tier_level,
+        file_version=src_visit.file_version,
+        project_file_id=src_visit.project_file_id,
+        removed=src_visit.removed,
+        batch_id=src_visit.batch_id,
+        record_num=src_visit.record_num)
+
+    return dst_med
+
+
+# --------------------------------------------------
+def create_visit_vital(src_vital, dst_visit):
+    """ Create a visit vital """
+
+    dst_vital, _ = dst.VisitVitals.get_or_create(
+        visit_vitals_id=src_vital.visit_vitals_id,
+        subject_visit_id=dst_visit.subject_visit_id,
+        height=src_vital.height,
+        weight=src_vital.weight,
+        bmi=src_vital.bmi,
+        tempurature=src_vital.tempurature,  # typo in schema, tempura -- yum!
+        pulse=src_vital.pulse,
+        respiratory_rate=src_vital.respiratory_rate,
+        systolic=src_vital.systolic,
+        diastolic=src_vital.diastolic,
+        tier_level=src_vital.tier_level,
+        file_version=src_vital.file_version,
+        project_file_id=src_vital.project_file_id,
+        removed=src_vital.removed,
+        batch_id=src_vital.batch_id,
+        record_num=src_vital.record_num)
+
+    return dst_vital
+
+
+# --------------------------------------------------
+def create_mmse(src_mmse, dst_visit):
+    """ Create MMSE """
+
+    dst_mmse, _ = dst.Mmse.get_or_create(
+        mmse_id=src_mmse.mmse_id,
+        subject_visit_id=dst_visit.subject_visit_id,
+        orient_time=src_mmse.orient_time,
+        orient_place=src_mmse.orient_place,
+        registration=src_mmse.registration,
+        attention_and_calc=src_mmse.attention_and_calc,
+        recall=src_mmse.recall,
+        naming=src_mmse.naming,
+        repetition=src_mmse.repetition,
+        reading=src_mmse.reading,
+        writing=src_mmse.writing,
+        drawing=src_mmse.drawing,
+        total_score=src_mmse.total_score,
+        derived_code=src_mmse.derived_code,
+        derived_data=src_mmse.derived_data,
+        tier_level=src_mmse.tier_level,
+        file_version=src_mmse.file_version,
+        project_file_id=src_mmse.project_file_id,
+        removed=src_mmse.removed,
+        batch_id=src_mmse.batch_id,
+        record_num=src_mmse.record_num)
+
+    return dst_mmse
+
+
+# --------------------------------------------------
+def create_lab_result(src_lab_result, dst_visit):
+    """ Create Lab Result """
+
+    dst_lab, _ = dst.LabResult.get_or_create(
+        lab_result_id=src_lab_result.lab_result_id,
+        subject_visit_id=dst_visit.subject_visit_id,
+        lab_test_id=src_lab_result.lab_test_id,
+        result=src_lab_result.result,
+        description=src_lab_result.description,
+        ref_range_ind=src_lab_result.ref_range_ind,
+        reason_not_done=src_lab_result.reason_not_done,
+        fasting=src_lab_result.fasting,
+        tier_level=src_lab_result.tier_level,
+        file_version=src_lab_result.file_version,
+        project_file_id=src_lab_result.project_file_id,
+        removed=src_lab_result.removed,
+        batch_id=src_lab_result.batch_id,
+        record_num=src_lab_result.record_num)
+
+    return dst_lab
 
 
 # --------------------------------------------------
